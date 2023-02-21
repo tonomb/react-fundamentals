c37125fe69ce36545bdad4df3c069159b6a68907
antonio martinez baez <tono.mtzb@gmail.com>
2023-02-20 21:29:24 -0600

    Exercise 6 finished

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,9 +2,30 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import { useRef, useState } from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+function UsernameForm({ onSubmitUsername }) {
   // 🐨 add a submit event handler here (`handleSubmit`).
+  const [errorState, setErrorState] = useState(false)
+
+
+  const usernameInput = useRef(null);
+
+  function onChangeHandler(e) {
+    let value = e.target.value;
+
+    if (value !== value.toLowerCase()) {
+      setErrorState(true)
+    } else {
+      setErrorState(false)
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const name = usernameInput.current.value
+    onSubmitUsername(name)
+  }
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -20,12 +41,13 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label >Username:</label>
+        <input type="text" name='username' ref={usernameInput} onChange={onChangeHandler} />
+        {errorState && <p style={{ color: "red" }}>Username must ve valid</p>}
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={errorState} >Submit</button>
     </form>
   )
 }
